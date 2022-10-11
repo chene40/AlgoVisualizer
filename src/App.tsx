4a3d8625e23bfde37b7e261012463e4e2b2dddb7
Eric Chen <chene9802@gmail.com>
2022-10-10 23:07:30 -0400

    Setup TailwindCSS as CSS utility library for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="bg-blue-200 h-10">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
